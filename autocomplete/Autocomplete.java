826db2a245b35a425e98540c8f2d3cceb1f5901d
Xu Zheng <xuzheng465@gmail.lcom>
2020-03-14 18:44:12 +1100

    Initial commit

---
+++ autocomplete/Autocomplete.java
@@ -0,0 +1,24 @@
+package autocomplete;
+
+public class Autocomplete {
+
+    // Initializes the data structure from the given array of terms.
+    public Autocomplete(Term[] terms) {
+    }
+
+    // Returns all terms that start with the given prefix, in descending order of weight.
+    public Term[] allMatches(String prefix) {
+        return null;
+    }
+
+
+    // Returns the number of terms that start with the given prefix.
+    public int numberOfMatches(String prefix) {
+        return 1;
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+    }
+
+}
---
+++ autocomplete/BinarySearchDeluxe.java
@@ -0,0 +1,22 @@
+package autocomplete;
+
+import java.util.Comparator;
+
+public class BinarySearchDeluxe {
+
+    // Returns the index of the first key in the sorted array a[]
+    // that is equal to the search key, or -1 if no such key.
+    public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
+        return 1;
+    }
+
+    // Returns the index of the last key in the sorted array a[]
+    // that is equal to the search key, or -1 if no such key.
+    public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
+        return 0;
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+    }
+}
