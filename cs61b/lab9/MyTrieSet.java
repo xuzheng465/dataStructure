08ffa60759b6b90b20636bc277807d36b8535714
Xu Zheng <xuzheng465@gmail.lcom>
2020-03-21 09:29:11 +1100

    add cs61b configure lab

---
+++ cs61b/lab9/MyTrieSet.java
@@ -0,0 +1,30 @@
+package cs61b.lab9;
+
+import java.util.List;
+
+public class MyTrieSet implements TrieSet61B {
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean contains(String key) {
+        return false;
+    }
+
+    @Override
+    public void add(String key) {
+
+    }
+
+    @Override
+    public List<String> keysWithPrefix(String prefix) {
+        return null;
+    }
+
+    @Override
+    public String longestPrefixOf(String key) {
+        return null;
+    }
+}
